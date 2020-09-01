a48101822717c168b514848abb7a458d540965e3
Mohamed Imad <email2emad@gmail.com>
2020-09-01 21:48:21 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,255 @@
+<?php 
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        html {
+            scroll-behavior: smooth !important;
+        }
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow">
+        <a class="navbar-brand" href="#home">Imadeous</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="#home">Home</a>
+                <a class="nav-item nav-link" href="#skills">Skills</a>
+                <a class="nav-item nav-link" href="#education">Education</a>
+                <a class="nav-item nav-link" href="#experience">Experience</a>
+            </div>
+        </div>
+    </nav>
+    <div id="home" class="jumbotron text-light" style="min-height: 400px; background-image: url('img/bg.png'); background-size: cover; backgroung-repeat: no-repeat;">
+        <div class="text-center mt-5">
+            <img src="img/avatar.png" width="150px" class="rounded-circle">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead"><b>This is the space in internet where I tell you about myself.</b></p>
+            <p>If you have any ideas and you think we can work together to make it a reality, don't wait!</p>
+            <a class="btn btn-outline-light" href="#" role="button" style="border-radius: 50px !important;" data-toggle="modal" data-target="#exampleModal">
+                Contact Me!
+            </a>
+        </div>
+    </div>
+    <hr class="mb-4">
+    <div class="container mb-3">
+        <h1 id="skills" class="text-center">What I do</h1>
+            <div class="card shadow mb-3">
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ut quae dolor ullam voluptatum in veniam dicta quam asperiores eaque odit voluptatibus molestiae autem dolorem sit perferendis saepe, odio aliquid.
+                </div>
+            </div>       
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="card shadow">
+                    <div style="height: 200px; overflow: hidden">   
+                        <img class="card-img-top" src="img/fullstack.png" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Full Stack Web Development</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>  
+            </div>
+            <div class="col-lg-4  mb-4">
+                <div class="card shadow">
+                    <div style="height: 200px; overflow: hidden">
+                    <img class="card-img-top" src="img/problem.png" alt="Card image cap">
+                    </div>
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">Problem Solving</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>  
+            </div> 
+            <div class="col-lg-4  mb-4">
+                <div class="card shadow">
+                    <div style="height: 200px; overflow: hidden">
+                    <img class="card-img-top" src="img/mentoring.png" alt="Card image cap">
+                    </div>
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">Mentoring</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>  
+            </div> 
+        </div>
+        <hr>
+        <h1 id="education" class="text-center">Education</h1>
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 mb-3">
+                <div class="card d-flex flex-md-row shadow">
+                    <div class="bg-white  align-self-center my-auto">
+                        <img src="img/ls.png" width="200px">
+                    </div>
+                    <div class="p-3 my-auto">
+                       <h4>28<sup>th</sup> Comprehensive Training Course<br><small class="text-muted">Indian Naval Academy<br>(2019 - Present)</small></h4>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, assumenda cupiditate nulla eligendi ullam perspiciatis inventore quo molestiae, porro cum labore. Odio, labore? Libero eveniet omnis dolorem, esse sed in?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 offset-lg-2 mb-3">
+                <div class="card d-flex flex-md-row-reverse shadow">
+                    <div class="bg-white my-auto align-self-center">
+                        <img src="img/infantry.png" width="200px">
+                    </div>
+                    <div class="p-3 my-auto">
+                    <h4>MNDF 63<sup>rd</sup> Basic Training Course<br><small class="text-muted">College of Defence and Security Studies<br>(2017)</small></h4>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, assumenda cupiditate nulla eligendi ullam perspiciatis inventore quo molestiae, porro cum labore. Odio, labore? Libero eveniet omnis dolorem, esse sed in?</p>
+                    </div>                    
+                </div>
+            </div>
+            <div class="col-lg-8 offset-lg-2 mb-3">
+                <div class="card d-flex flex-md-row shadow">
+                    <div class="bg-white  align-self-center my-auto">
+                        <img src="img/ks.png" width="200px">
+                    </div>
+                    <div class="p-3 my-auto">
+                       <h4>Edexcel A'Level & HSC<br><small class="text-muted">Kendhikulhudhoo School<br>(2012)</small></h4>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, assumenda cupiditate nulla eligendi ullam perspiciatis inventore quo molestiae, porro cum labore. Odio, labore? Libero eveniet omnis dolorem, esse sed in?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 offset-lg-2 mb-3">
+                <div class="card d-flex flex-md-row-reverse shadow">
+                    <div class="bg-white  align-self-center my-auto">
+                        <img src="img/fs.png" width="200px">
+                    </div>
+                    <div class="p-3  my-auto">
+                    <h4>IGCSE, GCE O'LEVEL & SSC<br><small class="text-muted">Funadhoo School<br>(2009)</small></h4>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, assumenda cupiditate nulla eligendi ullam perspiciatis inventore quo molestiae, porro cum labore. Odio, labore? Libero eveniet omnis dolorem, esse sed in?</p>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+        <hr>
+        <h1 id="experience" class="text-center">Work Experience</h1>
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 mb-3">
+                <div class="card d-flex flex-md-row-reverse shadow">
+                    <div class="bg-white my-auto align-self-center">
+                        <img src="img/ls.png" width="200px">
+                    </div>
+                    <div class="p-3 my-auto">
+                    <h4>Under Trainee Officer<br><small class="text-muted">MNDF<br>(2019 - Present)</small></h4>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, assumenda cupiditate nulla eligendi ullam perspiciatis inventore quo molestiae, porro cum labore. Odio, labore? Libero eveniet omnis dolorem, esse sed in?</p>
+                    </div>                    
+                </div>
+            </div>
+            <div class="col-lg-8 offset-lg-2 mb-3">
+                <div class="card d-flex flex-md-row shadow">
+                    <div class="bg-white  align-self-center my-auto">
+                        <img src="img/infantry.png" width="200px">
+                    </div>
+                    <div class="p-3 my-auto">
+                       <h4>Private<br><small class="text-muted">MNDF<br>(2016 - Present)</small></h4>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, assumenda cupiditate nulla eligendi ullam perspiciatis inventore quo molestiae, porro cum labore. Odio, labore? Libero eveniet omnis dolorem, esse sed in?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 offset-lg-2 mb-3">
+                <div class="card d-flex flex-md-row-reverse shadow">
+                    <div class="bg-white my-auto align-self-center">
+                        <img src="img/fs.png" width="200px">
+                    </div>
+                    <div class="p-3 my-auto">
+                    <h4>Assistant IT Technician<br><small class="text-muted">Funadhoo School<br>(2014 - 2016)</small></h4>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, assumenda cupiditate nulla eligendi ullam perspiciatis inventore quo molestiae, porro cum labore. Odio, labore? Libero eveniet omnis dolorem, esse sed in?</p>
+                    </div>                    
+                </div>
+            </div>
+            <div class="col-lg-8 offset-lg-2 mb-3">
+                <div class="card d-flex flex-md-row shadow">
+                    <div class="bg-white  align-self-center my-auto">
+                        <img src="img/mth.png" width="200px">
+                    </div>
+                    <div class="p-3 my-auto">
+                       <h4>Labourer<br><small class="text-muted">MT Højgaard Maldives<br>(2010)</small></h4>
+                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, assumenda cupiditate nulla eligendi ullam perspiciatis inventore quo molestiae, porro cum labore. Odio, labore? Libero eveniet omnis dolorem, esse sed in?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <footer class="bg-dark p-5 text-white">
+        <div class="row">
+            <div class="col-lg-6 mb-5 text-center my-auto">
+                <button class="btn btn-outline-light rounded-circle mr-1" style="width: 40px">
+                    <i class="fa fa-twitter"></i>
+                </button>
+                <button class="btn btn-outline-light rounded-circle  mr-1" style="width: 40px">
+                    <i class="fa fa-facebook"></i>
+                </button>
+                <button class="btn btn-outline-light rounded-circle  mr-1" style="width: 40px">
+                    <i class="fa fa-instagram"></i>
+                </button>
+                <button class="btn btn-outline-light rounded-circle  mr-1" style="width: 40px">
+                    <i class="fa fa-github"></i>
+                </button>
+                <button class="btn btn-outline-light rounded-circle mr-1" style="width: 40px">
+                    <i class="fa fa-google-plus"></i>
+                </button>
+            </div>
+            <div class="col-lg-6 mb-2 my-auto">
+                <div>
+                    <address>Silver Light, Sh. Funadhoo 03150,<br>Upper North Province, Maldives</address>
+                    Phone: <a href="#">9781993</a><br>
+                    Email: <a href="#">admin@imadeous.com</a> 
+                </div>   
+            </div>
+        </div>
+    </footer>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalCenterTitle">Contact Me!</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <form action="">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">I'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputSubject">Subject</label>
+                    <input type="text" class="form-control" id="exampleInputSubject" placeholder="Message Subject">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputBody">Message</label>
+                    <textarea class="form-control" id="exampleInputBody" placeholder="Your Message Here"></textarea>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-outline-primary " style="border-radius: 50px !important;"><i class="fa fa-send"></i> Send Message</button>
+        </div>
+        </div>
+    </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </div>
+    </div>
+  </body>
+</html>
